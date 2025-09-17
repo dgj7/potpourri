108bf427b17da7f0e8b6b3cc3c01df11f42008cf
dgj7 <garciad48879@gmail.com>
2025-09-17 19:55:26 -0400

    builds/c/static-lib: add 2 more demos; adjust makefile to build all separately

---
+++ builds/c/make-static-lib/examples/demo2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("this program doesn't even use the library");
+    return 0;
+}
---
+++ builds/c/make-static-lib/examples/demo3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "addition.h"
+
+int main() {
+    printf("%d+%d=%d", 101, 346, add(101, 346));
+    return 0;
+}
