ad04ac6a59f4ec7da7a832fc97904e25fcfb6c45
dgj7 <garciad48879@gmail.com>
2025-09-16 22:47:00 -0400

    create build (make) for simple c program

--- builds/c/make-simple-program/src/main.c
+++ builds/c/make-simple-program/src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "simple.h"
+#include "version.h"
 
 int main() {
     /* create some input variables; to make this really useful we'd use args, but nope not today */
@@ -8,7 +9,7 @@ int main() {
     int right = 2;
 
     /* print a message after calling the external function */
-    printf("%d + %d = %d", left, right, sum(left, right));
+    printf("%d + %d = %d", left, right, add(left, right));
 
     /* signal to the operating system that we didnt screw anything up */
     return 0;
--- builds/c/make-simple-program/src/simple.h
+++ builds/c/make-simple-program/src/simple.h
@@ -1,6 +1,14 @@
 #ifndef __SIMPLE_H__
 #define __SIMPLE_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int add(int left_addend, int right_addend);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
